f3b59b530f4aaed671a500df3d24c954a9d07c22
Kami <luisru693@gmail.com>
2023-11-20 16:32:52 -0500

    fashion calc trough arrays objects

--- themath.js
+++ themath.js
@@ -1,22 +1,82 @@
-// array [1, 2, 3, 4]
-function calcMedia(array){
-    // sum all array elements then divide them between the number of elements//
+function esPar(lista) {
+  return !(lista.length % 2);
+}
+function esImpar(lista) {
+  return lista.length % 2;
+}
 
-    // let arraySum = 0;
+function calcularModa(lista) {
+  const listaCount = {};
 
-    // for (let i = 0; i < array.length; i++) {
-    //     arraySum = arraySum + array[i];
-    // }
+  for (let i = 0; i < lista.length; i++) {
+    const elemento = lista[i];
 
-    function sumElements(acummulatedValue, newValue){
-        return acummulatedValue + newValue;
+    if (listaCount[elemento]) {
+      listaCount[elemento] += 1;
+    } else {
+      listaCount[elemento] = 1;
     }
+  }
 
-    const arraySum = array.reduce(sumElements);
+  const listaArray = Object.entries(listaCount);
+  const listaOrdenada = ordenarListaBidimensional(listaArray, 1)
+  const listaMaxNumber = listaOrdenada[listaOrdenada.length - 1];
+  const moda = listaMaxNumber[0];
+  // console.log({listaCount, listaArray, listaOrdenada, listaMaxNumber});
+  // console.log('La moda es: ' + listaMaxNumber[0]);
+  return moda;
+}
 
-    const media = arraySum / array.length;
-    console.log(media);
-    return media;
+function calcularMediana(listaDesordenada) {
+  const lista = ordenarLista(listaDesordenada);
+  const listaEsPar = esPar(lista);
 
-    // array.length //
+  if (listaEsPar) {
+    const indexMitad1ListaPar = (lista.length / 2) - 1;
+    const indexMitad2ListaPar = lista.length / 2;
+    const listaMitades = [];
+    listaMitades.push(lista[indexMitad1ListaPar]);
+    listaMitades.push(lista[indexMitad2ListaPar]);
+
+    const medianaListaPar = calcularPromedio(listaMitades);
+    return medianaListaPar;
+  } else {
+    const indexMitadListaImpar = Math.floor(lista.length / 2);
+    const medianaListaImpar = lista[indexMitadListaImpar];
+    console.log(indexMitadListaImpar);
+    console.log(medianaListaImpar);
+    return medianaListaImpar;
+  }
+}
+
+function calcularPromedio(lista) {
+  function sumarTodosElementos(valorAcumulado, nuevoValor) {
+    return valorAcumulado + nuevoValor;
+  }
+
+  const sumaLista = lista.reduce(sumarTodosElementos);  
+  const promedio = sumaLista / lista.length;
+  // console.log(promedio);
+  return promedio;
+}
+
+function ordenarLista(listaDesordenada) {
+  function ordenarListaSort(valorAcumulado, nuevoValor) {
+    return valorAcumulado - nuevoValor;
+  }
+  
+  // const lista = listaDesordenada.sort((a,b) => a-b);
+  const lista = listaDesordenada.sort(ordenarListaSort);
+  
+  return lista;
+}
+
+// [ [0,1],  [0,1],  [0,1] ]
+function ordenarListaBidimensional(listaDesordenada, i) {
+  function ordenarListaSort(valorAcumulado, nuevoValor) {
+    return valorAcumulado[i] - nuevoValor[i];
+  }
+  
+  const lista = listaDesordenada.sort(ordenarListaSort);
+  return lista;
 }
